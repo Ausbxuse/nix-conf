15e8baa79349da785b0b0a94815fb209c193e9d7
Zhenyu <peterzhaozzy@gmail.com>
2024-04-25 00:25:32 -0700

    44 current  2024-04-24 22:35:17  24.05pre615148.6143fc5eeb9c  6.8.7                           *

--- flake.nix
+++ flake.nix
@@ -1,6 +1,14 @@
 {
-  inputs.minegrub-theme.url = "github:Lxtharia/minegrub-theme";
-  outputs = {nixpkgs, ...} @ inputs: {
+  inputs = {
+    minegrub-theme.url = "github:Lxtharia/minegrub-theme";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    home-manager.url = "github:nix-community/home-manager";
+  };
+  outputs = {
+    self,
+    nixpkgs,
+    ...
+  } @ inputs: {
     nixosConfigurations.nixuse = nixpkgs.lib.nixosSystem {
       modules = [
         ./configuration.nix

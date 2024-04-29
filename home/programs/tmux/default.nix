decc888baf6589299bdc6be84e0f035ceecb8d3e
Zhenyu <peterzhaozzy@gmail.com>
2024-04-29 16:12:11 -0700

    81 current  2024-04-29 16:12:02  24.05.20240425.7bb2ccd       6.8.7                           *

--- home/programs/tmux/default.nix
+++ home/programs/tmux/default.nix
@@ -9,14 +9,15 @@
   '';
   programs.tmux = {
     enable = true;
+    terminal = "xterm-256color";
     extraConfig = ''
       run-shell ${pkgs.tmuxPlugins.cpu}/share/tmux-plugins/cpu/cpu.tmux
       run-shell ${pkgs.tmuxPlugins.sensible}/share/tmux-plugins/sensible/sensible.tmux
-      run-shell ${pkgs.tmuxPlugins.prefix-highlight}/share/tmux-plugins/prefix-highlight/prefix-highlight.tmux
+      run-shell ${pkgs.tmuxPlugins.prefix-highlight}/share/tmux-plugins/prefix-highlight/prefix_highlight.tmux
       run-shell ${pkgs.tmuxPlugins.battery}/share/tmux-plugins/battery/battery.tmux
       run-shell ${pkgs.tmuxPlugins.resurrect}/share/tmux-plugins/resurrect/resurrect.tmux
       run-shell ${pkgs.tmuxPlugins.continuum}/share/tmux-plugins/continuum/continuum.tmux
-      run-shell ${pkgs.tmuxPlugins.tmux-fzf}/share/tmux-plugins/tmux-fzf/tmux-fzf.tmux
+      run-shell ${pkgs.tmuxPlugins.tmux-fzf}/share/tmux-plugins/tmux-fzf/main.tmux
     '';
   };
 }

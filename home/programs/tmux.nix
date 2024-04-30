0075a252e44094496be9c7f5dea3d77bc9a70978
Zhenyu <peterzhaozzy@gmail.com>
2024-04-29 17:06:49 -0700

    tryig to fix tmux

--- home/programs/tmux.nix
+++ home/programs/tmux.nix
@@ -4,6 +4,9 @@
   pkgs,
   ...
 }: {
+  home.activation.installTmux = lib.hm.dag.entryAfter ["writeBoundary"] ''
+    ${pkgs.rsync}/bin/rsync -avz --chmod=D2755,F744 ${./tmux/plugins/tpm}/ ${config.xdg.configHome}/tmux/plugins/tpm/
+  '';
   home.file.".config/tmux" = {
     source = ./tmux;
     recursive = true;

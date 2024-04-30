cf080905cc25050e2c0fc594a866b43ac1d5dc15
Zhenyu <peterzhaozzy@gmail.com>
2024-04-29 17:44:54 -0700

    95 current  2024-04-29 17:44:47  24.05.20240425.7bb2ccd       6.8.7                           *

--- home/programs/zsh/default.nix
+++ home/programs/zsh/default.nix
@@ -7,16 +7,16 @@
   home.activation.installZsh = lib.hm.dag.entryAfter ["writeBoundary"] ''
     ${pkgs.rsync}/bin/rsync -avz --chmod=D2755,F744 ${./zsh}/ ${config.xdg.configHome}/zsh/
   '';
-  # programs.zsh = {
-  #   enable = true;
-  #   enableCompletion = true;
-  #   autosuggestion.enable = true;
-  #   syntaxHighlighting.enable = true;
+  programs.zsh = {
+    enable = true;
+    enableCompletion = true;
+    autosuggestion.enable = true;
+    syntaxHighlighting.enable = true;
 
-  #   shellAliases = {
-  #     ll = "ls -l";
-  #   };
-  #   history.size = 10000;
-  #   history.path = "${config.xdg.dataHome}/zsh/history";
-  # };
+    shellAliases = {
+      ll = "ls -l";
+    };
+    history.size = 10000;
+    history.path = "${config.xdg.dataHome}/zsh/history";
+  };
 }

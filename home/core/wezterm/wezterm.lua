6f952d7ef6bbd75c339f15dd2741801c352ca882
Zhenyu Zhao <peterzhaozzy@gmail.com>
2024-05-20 15:01:54 -0700

    Wezterm: add wezterm

---
+++ home/core/wezterm/wezterm.lua
@@ -0,0 +1,123 @@
+-- Pull in the wezterm API
+local wezterm = require("wezterm")
+
+-- This table will hold the configuration.
+local config = {}
+
+config.colors = {
+	-- The default text color
+	foreground = "#eaeaea",
+	-- The default background color
+	background = "#121212",
+
+	-- Overrides the cell background color when the current cell is occupied by the
+	-- cursor and the cursor style is set to Block
+	cursor_bg = "#000000",
+	-- Overrides the text color when the current cell is occupied by the cursor
+	cursor_fg = "#ffffff",
+	-- Specifies the border color of the cursor when the cursor style is set to Block,
+	-- or the color of the vertical or horizontal bar when the cursor style is set to
+	-- Bar or Underline.
+	cursor_border = "#eaffea",
+
+	-- the foreground color of selected text
+	selection_fg = "#eaeaea",
+	-- the background color of selected text
+	selection_bg = "#404040",
+
+	-- The color of the scrollbar "thumb"; the portion that represents the current viewport
+	scrollbar_thumb = "#222222",
+
+	-- The color of the split lines between panes
+	split = "#444444",
+
+	ansi = {
+		"#121212",
+		"#ff000f",
+		"#aae95b",
+		"#ffb900",
+		"#57c7ff",
+		"#ff6ac1",
+		"#00d7ea",
+		"#ffffff",
+	},
+	brights = {
+		"#444444",
+		"#ff2740",
+		"#abe15b",
+		"#ffd242",
+		"#61afef",
+		"#fc1a70",
+		"#67fff0",
+		"#ffffff",
+	},
+
+	-- Arbitrary colors of the palette in the range from 16 to 255
+	indexed = { [136] = "#af8700" },
+
+	-- Since: 20220319-142410-0fcdea07
+	-- When the IME, a dead key or a leader key are being processed and are effectively
+	-- holding input pending the result of input composition, change the cursor
+	-- to this color to give a visual cue about the compose state.
+	compose_cursor = "black",
+
+	-- Colors for copy_mode and quick_select
+	-- available since: 20220807-113146-c2fee766
+	-- In copy_mode, the color of the active text is:
+	-- 1. copy_mode_active_highlight_* if additional text was selected using the mouse
+	-- 2. selection_* otherwise
+	copy_mode_active_highlight_bg = { Color = "#000000" },
+	-- use `AnsiColor` to specify one of the ansi color palette values
+	-- (index 0-15) using one of the names "Black", "Maroon", "Green",
+	--  "Olive", "Navy", "Purple", "Teal", "Silver", "Grey", "Red", "Lime",
+	-- "Yellow", "Blue", "Fuchsia", "Aqua" or "White".
+	copy_mode_active_highlight_fg = { AnsiColor = "Black" },
+	copy_mode_inactive_highlight_bg = { Color = "#52ad70" },
+	copy_mode_inactive_highlight_fg = { AnsiColor = "White" },
+
+	quick_select_label_bg = { Color = "peru" },
+	quick_select_label_fg = { Color = "#ffffff" },
+	quick_select_match_bg = { AnsiColor = "Navy" },
+	quick_select_match_fg = { Color = "#ffffff" },
+}
+
+config.font_size = 10.0
+
+--[[ config.window_frame = {
+  -- The font used in the tab bar.
+  -- Roboto Bold is the default; this font is bundled
+  -- with wezterm.
+  -- Whatever font is selected here, it will have the
+  -- main font setting appended to it to pick up any
+  -- fallback fonts you may have used there.
+  font = wezterm.font { family = 'Roboto', weight = 'Bold' },
+
+  -- The size of the font in the tab bar.
+  -- Default to 10.0 on Windows but 12.0 on other systems
+  font_size = 10.0,
+
+  -- The overall background color of the tab bar when
+  -- the window is focused
+  active_titlebar_bg = '#333333',
+
+  -- The overall background color of the tab bar when
+  -- the window is not focused
+  inactive_titlebar_bg = '#333333',
+} ]]
+
+config.window_background_opacity = 0.8
+config.enable_wayland = true
+
+-- In newer versions of wezterm, use the config_builder which will
+-- help provide clearer error messages
+--[[ if wezterm.config_builder then
+  config = wezterm.config_builder()
+end ]]
+
+-- This is where you actually apply your config choices
+
+-- For example, changing the color scheme:
+-- config.color_scheme = 'AdventureTime'
+
+-- and finally, return the configuration to wezterm
+return config

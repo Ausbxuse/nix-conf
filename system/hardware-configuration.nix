c934bb5b138e94b0ee573237942d3f177b5ff572
Zhenyu Zhao <peterzhaozzy@gmail.com>
2024-05-27 22:50:06 -0700

    Clean up

--- system/hardware-configuration.nix
+++ system/hardware-configuration.nix
@@ -19,8 +19,6 @@
     };
   };
 
-  boot.plymouth.enable = true;
-  boot.plymouth.theme = "breeze";
   boot.initrd.availableKernelModules = ["xhci_pci" "thunderbolt" "vmd" "nvme" "usb_storage" "usbhid" "sd_mod"];
   boot.initrd.kernelModules = ["dm-snapshot"];
   boot.kernelModules = ["kvm-intel"];

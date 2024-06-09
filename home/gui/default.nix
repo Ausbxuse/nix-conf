fcf3dc3c06ace48349f2ed942bfed7229060a0fd
Zhenyu Zhao <peterzhaozzy@gmail.com>
2024-06-09 15:19:31 +0800

    xdg data dir: add themes

--- home/gui/default.nix
+++ home/gui/default.nix
@@ -4,10 +4,14 @@
   pkgs,
   ...
 }: {
+  home.activation.installThemes = lib.hm.dag.entryAfter ["writeBoundary"] ''
+    ${pkgs.rsync}/bin/rsync -avz --chmod=D2755,F744 ${./themes}/ ${config.xdg.dataHome}/themes/
+  '';
+
   home.activation.installDconf = lib.hm.dag.entryAfter ["writeBoundary"] ''
     ${pkgs.rsync}/bin/rsync -avz --chmod=D2755,F744 ${./dconf}/ ${config.xdg.configHome}/dconf/
-
   '';
+
   home.activation.installFonts = lib.hm.dag.entryAfter ["writeBoundary"] ''
     ${pkgs.rsync}/bin/rsync -avz --chmod=D2755,F744 ${./fonts}/ ${config.xdg.dataHome}/fonts/
   '';

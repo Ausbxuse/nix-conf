4e90931206dd6b67b3b5b7b15792e118ebc7c239
Zhenyu Zhao <peterzhaozzy@gmail.com>
2024-09-14 12:22:22 -0700

    Timy: Add alacritty

--- hosts/base/home/gui/default.nix
+++ hosts/base/home/gui/default.nix
@@ -35,6 +35,143 @@
     ${pkgs.rsync}/bin/rsync -avz --chmod=D2755,F744 ${./wallpapers}/ ${config.xdg.dataHome}/wallpapers/
   '';
   programs = {
+    alacritty = {
+      enable = true;
+      settings = {
+        colors = {
+          bright = {
+            black = "#444444";
+            blue = "#4fc1ff";
+            cyan = "#62d8f1";
+            green = "#9ece6a";
+            magenta = "#fc317e";
+            red = "#ff2740";
+            white = "#dfdcd8";
+            yellow = "#f4bf75";
+          };
+          cursor = {
+            cursor = "#eaeaea";
+            text = "#000000";
+          };
+          dim = {
+            black = "#000000";
+            blue = "#506d8f";
+            cyan = "#497e7a";
+            green = "#7a8530";
+            magenta = "#80638e";
+            red = "#8c3336";
+            white = "#eaeaea";
+            yellow = "#97822e";
+          };
+          normal = {
+            black = "#121212";
+            blue = "#4fc1ff";
+            cyan = "#62d8f1";
+            green = "#9ece6a";
+            magenta = "#fc317e";
+            red = "#ff2740";
+            white = "#dfdcd8";
+            yellow = "#f4bf75";
+          };
+          primary = {
+            background = "#121212";
+            foreground = "#dfdcd8";
+          };
+          selection = {
+            background = "#404040";
+            text = "#dfdcd8";
+          };
+        };
+        cursor = {
+          style = {
+            blinking = "Never";
+          };
+        };
+        env = {
+          TERM = "xterm-256color";
+        };
+        font = {
+          size = 12;
+          bold = {
+            family = "JetBrainsMono NF";
+            style = "Bold";
+          };
+          bold_italic = {
+            family = "JetBrainsMono NF";
+            style = "Bold Italic";
+          };
+          italic = {
+            family = "Operator Mono Book";
+            style = "Italic";
+          };
+          normal = {
+            family = "JetBrainsMono NF";
+            style = "Regular";
+          };
+          offset = {
+            x = 0;
+            y = 0;
+          };
+        };
+        keyboard = {
+          bindings = [
+            {
+              action = "Paste";
+              key = "V";
+              mods = "Command";
+            }
+            {
+              action = "Copy";
+              key = "C";
+              mods = "Command";
+            }
+            {
+              action = "PasteSelection";
+              key = "Insert";
+              mods = "Shift";
+            }
+            {
+              action = "ResetFontSize";
+              key = "Key0";
+              mods = "Control";
+            }
+            {
+              action = "IncreaseFontSize";
+              key = "Equals";
+              mods = "Control";
+            }
+            {
+              action = "DecreaseFontSize";
+              key = "Minus";
+              mods = "Control";
+            }
+            {
+              action = "ScrollPageUp";
+              key = "PageUp";
+              mode = "~Alt";
+            }
+            {
+              action = "ScrollPageDown";
+              key = "PageDown";
+              mode = "~Alt";
+            }
+          ];
+        };
+        scrolling = {
+          history = 10000;
+          multiplier = 1;
+        };
+        window = {
+          dynamic_padding = true;
+          opacity = 0.70;
+          decorations = "None";
+        };
+        mouse = {
+          hide_when_typing = false;
+        };
+      };
+    };
+
     wezterm = {
       enable = true;
       enableZshIntegration = true;
--- hosts/uni/home/gui/dconf.nix
+++ hosts/uni/home/gui/dconf.nix
@@ -1,9 +1,4 @@
 # Generated via dconf2nix: https://github.com/gvolpe/dconf2nix
 {lib, ...}:
 with lib.hm.gvariant; {
-  dconf.settings."org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom2" = {
-    binding = "<Super>space";
-    command = "alacritty";
-    name = "Terminal";
-  };
 }

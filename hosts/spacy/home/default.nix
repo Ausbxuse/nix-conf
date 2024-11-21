a86b336c39368fc82f17bb4344a2a276f56653ea
Zhenyu Zhao <peterzhaozzy@gmail.com>
2024-11-21 15:54:38 -0800

    Tmux: fixes

--- hosts/spacy/home/default.nix
+++ hosts/spacy/home/default.nix
@@ -2,8 +2,6 @@
   config,
   lib,
   pkgs,
-  username,
-  user-homedir,
   ...
 }: {
   imports = [
@@ -12,7 +10,7 @@
 
   # Overrides
   programs.zsh.initExtra = builtins.readFile ./tui/zsh-config/zshrc;
-  programs.tmux.extraConfig = builtins.readFile ./tui/tmux-config/tmux.conf;
+  programs.tmux.extraConfig = builtins.readFile ../../base/home/tui/tmux-config/tmux.conf + "\n" + builtins.readFile ./tui/tmux-config/tmux.conf;
 
   home.activation.installStartupScript = lib.hm.dag.entryAfter ["writeBoundary"] ''
     ${pkgs.rsync}/bin/rsync -avz --chmod=D2755,F744 ${./tui/bin}/ ${config.home.homeDirectory}/.local/bin/

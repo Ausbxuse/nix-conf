94b579452e92e29828a72e4ae4ff07171b9ed53b
Zhenyu Zhao <peterzhaozzy@gmail.com>
2025-01-20 13:42:31 -0800

    fix

--- modules/home/nvim/nvim/lua/config/autocmds.lua
+++ modules/home/nvim/nvim/lua/config/autocmds.lua
@@ -62,6 +62,49 @@ vim.api.nvim_create_autocmd('BufEnter', {
   end,
 })
 
+vim.api.nvim_create_autocmd('VimResized', { command = 'wincmd =' })
+
+-- Restore cursor pos
 vim.cmd [[
     autocmd BufReadPost * if line("'\"") >= 1 && line("'\"") <= line("$") && &ft !~# 'commit'  |   exe "normal! g`\""  | endif
 ]]
+
+-- Keep the cursor position when yanking
+local cursorPreYank
+
+vim.keymap.set({ 'n', 'x' }, 'y', function()
+  cursorPreYank = vim.api.nvim_win_get_cursor(0)
+  return 'y'
+end, { expr = true })
+
+vim.keymap.set('n', 'Y', function()
+  cursorPreYank = vim.api.nvim_win_get_cursor(0)
+  return 'y$'
+end, { expr = true })
+
+vim.api.nvim_create_autocmd('TextYankPost', {
+  callback = function()
+    if vim.v.event.operator == 'y' and cursorPreYank then
+      vim.api.nvim_win_set_cursor(0, cursorPreYank)
+    end
+  end,
+})
+
+-- Show cursorline only on active windows
+vim.api.nvim_create_autocmd({ 'InsertLeave', 'WinEnter' }, {
+  callback = function()
+    if vim.w.auto_cursorline then
+      vim.wo.cursorline = true
+      vim.w.auto_cursorline = false
+    end
+  end,
+})
+
+vim.api.nvim_create_autocmd({ 'InsertEnter', 'WinLeave' }, {
+  callback = function()
+    if vim.wo.cursorline then
+      vim.w.auto_cursorline = true
+      vim.wo.cursorline = false
+    end
+  end,
+})

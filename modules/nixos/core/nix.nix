492d182c0ae694fba92e85570979f46afd8099f1
Zhenyu Zhao <peterzhaozzy@gmail.com>
2025-01-26 21:29:11 -0800

    small fix

--- modules/nixos/core/nix.nix
+++ modules/nixos/core/nix.nix
@@ -33,9 +33,9 @@
     trusted-users = ["${username}"];
 
     substituters = [
-      "https://cuda-maintainers.cachix.org"
       "https://cache.nixos.org"
       "https://ausbxuse.cachix.org"
+      "https://nix-community.cachix.org"
       "https://wezterm.cachix.org"
       # cache mirror located in China
       #"https://mirror.sjtu.edu.cn/nix-channels/store"
@@ -45,7 +45,6 @@
     ];
     trusted-public-keys = [
       "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
-      "cuda-maintainers.cachix.org-1:0dq3bujKpuEPMCX6U4WylrUDZ9JyUG0VpVZa7CNfq5E="
       "wezterm.cachix.org-1:kAbhjYUC9qvblTE+s7S+kl5XM1zVa4skO+E/1IDWdH0="
     ];
   };

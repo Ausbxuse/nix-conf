13ed8ab18c1199b5dc2127323a168b79176be5fb
Zhenyu Zhao <peterzhaozzy@gmail.com>
2025-02-10 23:15:30 -0800

    fix home script

--- scripts/install_home.sh
+++ scripts/install_home.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/env bash
 
+
+sed -i \
+  -e "s/username = \"[^\"]*\";/username = \"$(whoami)\";/g" \
+  -e "s|user-homedir = \"/home/[^\"]*\";|user-homedir = \"$(echo $HOME)\";|g" \
+  -e "s/hostname = \"[^\"]*\";/hostname = \"$(hostname)\";/g" \
+  ./flake.nix
 mkdir -p ~/.config/nix && echo 'extra-experimental-features = nix-command flakes' >>~/.config/nix/nix.conf
 nix run nixpkgs#home-manager -- switch --flake .#earthy

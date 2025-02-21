20139c16776c4b6685d42d057c32582e287fabc1
Zhenyu Zhao <peterzhaozzy@gmail.com>
2025-02-21 01:14:07 -0800

    dynamic username in startup

--- modules/home/autostart/default.nix
+++ modules/home/autostart/default.nix
@@ -2,9 +2,30 @@
   config,
   lib,
   pkgs,
+  username,
   ...
 }: {
-  home.activation.installAutostart = lib.hm.dag.entryAfter ["writeBoundary"] ''
-    ${pkgs.rsync}/bin/rsync -avz --chmod=D2755,F644 ${./autostart}/ ${config.xdg.configHome}/autostart/
-  '';
+  home.file."${config.xdg.configHome}/autostart/startup.desktop" = {
+    text = ''
+      [Desktop Entry]
+      Type=Application
+      Exec=/home/${username}/.local/bin/startup
+      Icon=Terminal
+      Terminal=false
+      Categories=System;TerminalEmulator;
+
+      Name=Startup
+      GenericName=Terminal
+      Comment=My personal startup script
+      StartupWMClass=Startup
+      Actions=New;
+      X-GNOME-Autostart-enabled=true
+      X-Desktop-File-Install-Version=0.26
+
+      [Desktop Action New]
+      Name=New Terminal
+      Exec=/home/${username}/.local/bin/startup
+    '';
+    executable = false;
+  };
 }
